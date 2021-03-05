ca21a89539e802200a55f66ef461737feec68cac
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-03-05 23:48:49 +0530

    Temperature Conversion
    Celsius to Fahrenheit & Vice-versa.

---
+++ CtoF.py
@@ -0,0 +1,7 @@
+C = eval(input("Enter Temp in Celsius: "))
+F = (9*C/5)+32
+print("in Celsius: ",C,"\nin Fahrnehiet: ",F)
+F = eval(input("Enter Temp in Fahrnehiet: "))
+C = (F-32)*5/9
+print("in Celsius: ",C,"\nin Fahrnehiet: ",F)
+
