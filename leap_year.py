dcc37d2f2fbc7824004f02c920a414b7587906a0
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-03-17 14:41:00 +0530

    Delete leap_year.py

--- leap_year.py
+++
@@ -1,6 +0,0 @@
-input_year= int(input("Enter the year: "))
-
-if input_year%4==0 and input_year%100!=0 or input_year%400==0:
-    print("The year is a leap year.")
-else:
-    print("The year is not a leap year.")
