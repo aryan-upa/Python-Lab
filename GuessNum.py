67f04451b072485917bd5df1126766f33a2edf5c
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-06-24 00:35:36 +0530

    Guess the Number game

--- GuessNum.py
+++ GuessNum.py
@@ -1,13 +1,18 @@
 import random
 
-a = random.randrange(10)
+a = random.randrange(1,101)
 # print(a)
 loop = [1]
 for i in loop:
-    inp = int(input("Enter the Input: "))
+    inp = int(input("Guess: "))
     if a==inp:
         print("Well Guessed!")
+        print('Score',len(loop))
         exit()
     else:
-        print("Wrong Guess, Try again!")
+        if inp>a:
+            print('Greater..')
+        else:
+            print('Smaller..')
+        # print("Wrong Guess, Try again!")
         loop.append(1)
