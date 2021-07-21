c263a39a155367fee36935dd58af5c89ee3608ce
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-07-21 15:58:25 +0530

    New Badminton Series Solution

--- badminton tournaments series.py
+++ badminton tournaments series.py
@@ -2,3 +2,9 @@ ls = list(input().split(','))
 ls.sort()
 p = [ls.count(q) for q in ls]
 print(ls[p.index(max(p))])
+
+# Alternate Solution
+
+ls = sorted(input().split(',')); _ = ''
+for q in ls : _ = q if ls.count(q) > ls.count(_) else _
+print(_)
